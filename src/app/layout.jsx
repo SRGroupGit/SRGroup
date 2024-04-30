e2fd5098cfb04dcca78d588a9e6b374d16cae1c8
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2024-05-01 03:06:10 +0530

    fonts and basic setup

---
+++ src/app/layout.jsx
@@ -0,0 +1,18 @@
+import GlobalNavBar from './GlobalNavBar';
+import './globals.css';
+
+export const metadata = {
+  title: 'SR Group',
+  description: 'SR Group',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className=' font-sans '>
+        <GlobalNavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className=' font-bold'>deede</h1>
+      <h2 className=' font-light'>ddccd</h2>
+    </main>
+  );
+}
