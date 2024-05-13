122a6c0db4783146840681fb314de293f6125815
siddhant <siddhantvij001@gmail.com>
2024-05-13 11:45:58 +0530

    test

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/navigation';
 
 export default function About() {
   return (
-    <div className=' flex h-dvh w-full items-center justify-center'>
+    <div className=' flex h-dvh w-full items-center justify-center bg-neutral-200'>
       coming Soon
     </div>
   );
